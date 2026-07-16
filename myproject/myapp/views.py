7b30e741b4bf905a32ff52a2051177dbf478f6c9
vaishnavij7770 <vaishuj7770@gmail.com>
2026-07-16 16:20:40 +0530

    Add initial app structure with views and URL routing

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Im Home page")
+
+def about(request):
+    return HttpResponse("Im about page")
+
+def contact(request):
+    return HttpResponse("contact us")
