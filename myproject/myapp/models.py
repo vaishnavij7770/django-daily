d3e58381cb829bff2be90ab04e7fdec38762b0a0
vaishnavij7770 <vaishuj7770@gmail.com>
2026-07-17 16:46:36 +0530

    Add models, views, and templates for student and teacher management
    - Created `student` and `teacher` models with relevant fields.
    - Registered models in the Django admin.
    - Implemented views for home and about pages, rendering HTML templates.
    - Added HTML templates for home and about pages.
    - Created initial migration files for the new models.

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    fullname=models.CharField(max_length=100)  #attributes
+    email=models.EmailField(max_length=10)
+    contact=models.CharField(max_length=50)
+    city=models.CharField(max_length=10)
+
+class teacher(models.Model):
+    fullname=models.CharField(max_length=100)  #attributes
+    email=models.EmailField(max_length=20)
+    contact=models.CharField(max_length=50)
+    city=models.CharField(max_length=10)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,10 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Im Home page")
+    # return HttpResponse("Im Home page")
+    return render(request,"home.html")
 
 def about(request):
-    return HttpResponse("Im about page")
+    # return HttpResponse("Im about page")
+    return render(request,"about.html")
 
 def contact(request):
     return HttpResponse("contact us")
