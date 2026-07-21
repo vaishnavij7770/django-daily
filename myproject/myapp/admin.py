773efd6974b9dfc47e079440bf713702b277e1e8
vaishnavij7770 <vaishuj7770@gmail.com>
2026-07-21 15:28:18 +0530

    Add employee, user, member, and hero models with attributes; update admin registration

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import student, teacher
+from .models import student, teacher, emp, users, heroes, members
 # Register your models here.
 
-admin.site.register([student])
+admin.site.register([student,teacher,emp,users,heroes,members])
 
-admin.site.register([teacher])
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -12,3 +12,28 @@ class teacher(models.Model):
     email=models.EmailField(max_length=20)
     contact=models.CharField(max_length=50)
     city=models.CharField(max_length=10)
+
+
+class emp(models.Model):
+    fullname=models.CharField(max_length=100)  #attributes
+    email=models.EmailField(max_length=20)
+    contact=models.CharField(max_length=50)
+    city=models.CharField(max_length=10)
+
+class users(models.Model):
+    fullname=models.CharField(max_length=100)  #attributes
+    email=models.EmailField(max_length=20)
+    contact=models.CharField(max_length=50)
+    city=models.CharField(max_length=10)
+
+class members(models.Model):
+    fullname=models.CharField(max_length=100)  #attributes
+    email=models.EmailField(max_length=20)
+    contact=models.CharField(max_length=50)
+    city=models.CharField(max_length=10)
+
+class heroes(models.Model):
+    fullname=models.CharField(max_length=100)  #attributes
+    email=models.EmailField(max_length=20)
+    contact=models.CharField(max_length=50)
+    city=models.CharField(max_length=10)
