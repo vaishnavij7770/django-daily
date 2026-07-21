8b678266b7887ee5ab3c16292d6ba78274232df8
vaishnavij7770 <vaishuj7770@gmail.com>
2026-07-21 17:07:24 +0530

    Add register model to admin site registration

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import student, teacher, emp, users, heroes, members
+from .models import student, teacher, emp, users, heroes, members, register
 # Register your models here.
 
-admin.site.register([student,teacher,emp,users,heroes,members])
+admin.site.register([student,teacher,emp,users,heroes,members,register])
 
